3d2ca6497ae7869089c02d74812a94cd6cc457c4
hiem55 <oh.justin55@gmail.com>
2024-05-03 13:42:25 -0700

    Modify eslint configurations (#8)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,26 +5,8 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
-    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-    {
-        languageOptions: {
-            globals: {
-                console: 'readonly',  // Fixes 'console is not defined'
-                React: 'writable',  // Define React as writable
-                window: 'readonly',  // Browser global
-                document: 'readonly',  // Browser global
-                module: 'writable',  // Node.js global
-                process: 'writable',  // Node.js global
-            },
-        },
-        ignores: ['*.py', 'node_modules/'],  // Ignore Python files
-
-        rules: {
-            'semi': ['error', 'always'],  // Require semicolons
-            'quotes': ['error', 'double'],  // Enforce double quotes
-            'indent': ['error', 2],  // Enforce 2-space indentation
-        },
-    }, pluginJs.configs.recommended,
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReactConfig,
 ];
