73b4bf4c1ff4d342332dbfacfa79648f825aa0bf
hiem55 <oh.justin55@gmail.com>
2024-05-08 14:15:49 -0700

    update eslint rules (#8)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,9 +4,23 @@ import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
-export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReactConfig,
-];
+export default {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+    },
+    extends: [
+        pluginJs.configs.recommended,
+        ...tseslint.configs.recommended,
+        pluginReactConfig,
+    ],
+    rules: {
+        'react/react-in-jsx-scope': 'off',  // No need for React import with modern React
+    },
+    settings: {
+        react: {
+            version: 'detect',  // ESLint auto-detects React version
+        },
+    },
+};
