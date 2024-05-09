a870cfc57c5519051cfe5644a17043b5d13ee4b4
hiem55 <100002888+hiem55@users.noreply.github.com>
2024-05-09 12:40:41 -0700

    Update eslint config (#8)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,12 +6,18 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 export default [
     { languageOptions: { globals: globals.browser } },
+
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReactConfig,
     {
+        env: {
+            node: true,
+            es6: true,
+        },
         rules: {
             'react/react-in-jsx-scope': 'off',  // No need for React import with modern React
+            '@typescript-eslint/no-var-requires': 'off'
         },
         settings: {
             react: {
