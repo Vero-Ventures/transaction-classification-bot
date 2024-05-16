dfe73d455f9a8af4835cd994ba2ded3b046f2eb8
Mikko Sio <msio1@my.bcit.ca>
2024-05-16 13:10:50 -0700

    fixed purchase route

--- frontend/app/api/quickbooks/get_purchases/route.ts
+++ frontend/app/api/quickbooks/get_purchases/route.ts
@@ -2,5 +2,5 @@ import { get_purchases } from '../server_actions/app';
 
 export async function GET() {
   const purchases = await get_purchases();
-  return Response.json(JSON.parse(purchases));
+  return Response.json(purchases);
 }
