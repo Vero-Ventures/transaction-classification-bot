9c06df248439af6f3080dd8fd1688b8f17661977
Braden R <brogers42@my.bcit.ca>
2024-05-16 12:53:18 -0700

    Separated out creation of the qb client to a different server action file.

---
+++ frontend/actions/qb_client.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+const QB = require('node-quickbooks');
+
+
+// Create a QuickBooks client object.
+export async function create_qb_object() {
+
+    // Get the server session and save it as a constant.
+    const session = await getServerSession(options);
+
+    // Record the server session values.
+    const oauthToken = session?.accessToken;
+    const realmId = session?.realmId;
+    const refreshToken = session?.refreshToken;
+
+    // Create the QuickBooks API calls object.
+    const qbo = new QB(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        oauthToken,
+        false,
+        realmId,
+        true,
+        true,
+        null,
+        '2.0',
+        refreshToken,
+    );
+
+    // Return the QuickBooks API calls object.
+    return qbo;
+}
