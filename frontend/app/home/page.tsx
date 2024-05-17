ae4398bae9ad9f332c67caccd6b061df30f13397
Mikko Sio <msio1@my.bcit.ca>
2024-05-17 13:03:27 -0700

    integrated classify function

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -2,63 +2,52 @@
 
 import SelectionPage from "@/app/home/selection";
 import ReviewPage from "@/app/home/review";
-import { Purchase } from "@/interfaces/purchase";
 import { useEffect, useState } from "react";
-import { get_accounts, get_purchases } from "../api/quickbooks/server_actions/app";
-import { filterPurchases } from "@/utils/filter-uncategorized-purchases";
+import { get_transactions } from "../api/quickbooks/server_actions/app";
+import { filterCategorized, filterUncategorized } from "@/utils/filter-transactions";
+import { classifyTransactions } from "@/actions/classify";
+import { Transaction } from "@/types/Transaction";
 
 export default function HomePage() {
-  const [purchases, setPurchases] = useState<Purchase[]>([]);
+  const [purchases, setPurchases] = useState<Transaction[]>([]);
   const [categorizedResults, setCategorizedResults] = useState<any>({});
-  const [accounts, setAccounts] = useState<any[]>([]);
-  const [selectedPurchases, setSelectedPurchases] = useState<Purchase[]>([]);
+  const [selectedPurchases, setSelectedPurchases] = useState<Transaction[]>([]);
 
   useEffect(() => {
     const fetchPurchases = async () => {
       try {
-        const result = await get_purchases();
-        if (result) {
-          setPurchases(filterPurchases(result));
+        const response = await get_transactions();
+        const result = JSON.parse(response);
+        if (result[0].result === 'Success') {
+          setPurchases(result.slice(1));
         }
       } catch (error) {
         console.error('Error fetching purchases:', error);
       }
     }
 
-    const fetchAccounts = async () => {
-      try {
-        const result = await get_accounts();
-        if (result) {
-          setAccounts(JSON.parse(result));
-        }
-      } catch (error) {
-        console.error('Error fetching accounts:', error);
-      }
-    }
-
     fetchPurchases();
-    fetchAccounts();
   }, []);
 
-  const handleSubmit = (selectedPurchases: Purchase[]) => {
-    let testResults: { [key: string]: string[] } = {};
-    let possibleCategories: any = [];
-    accounts.forEach((account) => {
-      if (account.name) {
-        possibleCategories.push(account.name);
-      }
-    });
-    selectedPurchases.forEach((purchase) => {
-      testResults[purchase.Id] = possibleCategories;
-    });
-    setCategorizedResults(testResults);
+  const handleSubmit = async (selectedPurchases: Transaction[]) => {
+    const categorizedOnly: Transaction[] = filterCategorized(purchases);
+    const result = await classifyTransactions(categorizedOnly, filterUncategorized(selectedPurchases));
+    
+    if (result.data) {
+      const classifications: any = {};
+      result.data.forEach((classification) => {
+        classifications[classification.transaction_ID] = classification.possibleCategories;
+      });
+      
+      setCategorizedResults(classifications);
+    }
   };
 
   return (
     categorizedResults && Object.keys(categorizedResults).length > 0 ? (
       <ReviewPage selectedPurchases={selectedPurchases} categorizedResults={categorizedResults} />
     ) : (
-      <SelectionPage purchases={purchases} handleSubmit={handleSubmit} selectedPurchases={selectedPurchases} setSelectedPurchases={setSelectedPurchases} />
+      <SelectionPage purchases={filterUncategorized(purchases)} handleSubmit={handleSubmit} selectedPurchases={selectedPurchases} setSelectedPurchases={setSelectedPurchases} />
     )
   );
 }
