37efc26b625c607dd654ebb88c3f72351d0e0dbd
Braden R <brogers42@my.bcit.ca>
2024-05-21 12:37:35 -0700

    Use react use state to control button CSS to only show for logged in users.

--- frontend/components/signout-button.tsx
+++ frontend/components/signout-button.tsx
@@ -1,12 +1,22 @@
 'use client';
-
+import { getSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 const SignOutButton = () => {
+  const [buttonClass, setButtonClass] = useState<string>('hidden');
+  // Check if the user is signed in.
+  getSession().then(session => {
+    if (session) {
+      setButtonClass(
+        'text-white bg-red-500 hover:bg-red-600 px-3 py-1.5 rounded transition-colors duration-150'
+      );
+    }
+  });
   return (
     <button
-      className="text-white bg-red-500 hover:bg-red-600 px-3 py-1.5 rounded transition-colors duration-150"
-      onClick={() => signOut({ callbackUrl: 'http://localhost:3000' })}>
+      className={buttonClass}
+      onClick={() => signOut({ callbackUrl: '/' })}>
       Sign Out
     </button>
   );
