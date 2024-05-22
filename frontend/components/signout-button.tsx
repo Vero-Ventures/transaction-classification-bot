eba7ef05d69f1ab412da9941465dfa36852547a3
shawnbirring <shawnbirring21@gmail.com>
2024-05-22 10:46:04 -0700

    removed states of sign out button to remove delay

--- frontend/components/signout-button.tsx
+++ frontend/components/signout-button.tsx
@@ -1,21 +1,9 @@
 'use client';
-import { getSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
-import { useState } from 'react';
-
 const SignOutButton = () => {
-  const [buttonClass, setButtonClass] = useState<string>('hidden');
-  // Check if the user is signed in.
-  getSession().then(session => {
-    if (session) {
-      setButtonClass(
-        'text-white bg-red-500 hover:bg-red-600 px-3 py-1.5 rounded transition-colors duration-150'
-      );
-    }
-  });
   return (
     <button
-      className={buttonClass}
+      className="text-white bg-red-500 hover:bg-red-600 px-3 py-1.5 rounded transition-colors duration-150"
       onClick={() => signOut({ callbackUrl: '/' })}>
       Sign Out
     </button>
