2a487934f32c1eea9646c6e7c0b294fb5fe89fa9
hiem55 <oh.justin55@gmail.com>
2024-05-22 16:45:24 -0700

    add logout and redirect test (#67)

--- playwright-tests/tests/landingpage.spec.ts
+++ playwright-tests/tests/landingpage.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { config } from './config';
+import { authenticate } from './authhelper';
 
-test('landing page', async ({ page }) => {
+test('landing page and logout button', async ({ page }) => {
   await page.goto('https://transaction-classification-bot.vercel.app/');
 
   // Check if the "Intuit Sign-In" button is present
@@ -9,6 +10,10 @@ test('landing page', async ({ page }) => {
   if (signInButton) {
     const signInButtonText = await signInButton.textContent();
     expect(signInButtonText).toBe('Intuit Sign-In');
+
+    // When not logged in, there should be no logout button
+    const logoutButton = await page.$('text=Logout');
+    expect(logoutButton).toBeFalsy();
   } else {
     // Check for the "Proceed to Home Page" button if "Intuit Sign-In" is not found
     const proceedButton = await page.$('text=Proceed to Home Page');
@@ -18,6 +23,9 @@ test('landing page', async ({ page }) => {
       const proceedButtonText = await proceedButton.textContent();
       expect(proceedButtonText).toBe('Proceed to Home Page');
     }
+    // Check for the logout button if the user is already logged in
+    const logoutButton = await page.$('text=Sign Out');
+    expect(logoutButton).toBeTruthy();
   }
 });
 
@@ -72,7 +80,6 @@ test('User sign-in functionality', async ({ page }) => {
 
   // click on the continue button
   await page.click('button[data-testid="passwordVerificationContinueButton"]');
-
   // this will lead to phone or email verification which we will consider has passed the login test
 });
 
@@ -80,7 +87,7 @@ test('User redirect to login page when not logged in', async ({ page }) => {
   // Navigate to the home page
   await page.goto('https://transaction-classification-bot.vercel.app/home');
   await page.waitForURL(
-    'https://transaction-classification-bot.vercel.app/api/auth/signin'
+    'https://transaction-classification-bot.vercel.app/api/auth/signin?callbackUrl=%2Fhome'
   );
 
   // Check if the user is redirected to the login page
@@ -88,16 +95,51 @@ test('User redirect to login page when not logged in', async ({ page }) => {
   expect(signInButton).toBeTruthy();
 });
 
-test('User remains on home page when already logged in', async ({ page }) => {
-  // Navigate to the home page
-  await page.goto('https://transaction-classification-bot.vercel.app/home');
+// TEST COMMENTED OUT DUE TO AUTHENTICATED STATE NOT PERSISTING ACROSS TESTS
+// test.use({ storageState: process.env.USER_AUTH });
 
-  // Check if specific elements unique to the home page are present
-  const homePageElement = await page.$('text=My Expenses');
-  expect(homePageElement).toBeTruthy();
+// test('User remains on home page when already logged in', async ({ page }) => {
+//     // Navigate to the home page
+//     await page.goto('https://transaction-classification-bot.vercel.app/home');
 
-  // Check if the URL remains on the home page
-  expect(page.url()).toBe(
-    'https://transaction-classification-bot.vercel.app/home'
+//     // Check if specific elements unique to the home page are present
+//     const homePageElement = await page.$('text=My Expenses');
+//     expect(homePageElement).toBeTruthy();
+
+//     // Check if the URL remains on the home page
+//     expect(page.url()).toBe(
+//         'https://transaction-classification-bot.vercel.app/home'
+//     );
+// });
+
+// COMMENTED OUT DUE TO AUTHENTICATED STATE NOT PERSISTING ACROSS TESTS
+//test.use({ storageState: process.env.USER_AUTH });
+
+// TEST REQUIRES USER TO PUT IN A await page.pause() COMMAND TO MANUALLY VERIFY PHONE / EMAIL ONE TIME BEFORE THE AUTHENTICATION SKIPS 2FA CHECK.
+test('logged in user clicks logout button', async ({ page }) => {
+  const info = {
+    USER_EMAIL: config.USER_EMAIL,
+    USER_PASSWORD: config.USER_PASSWORD,
+  };
+  await authenticate(page, info);
+
+  // Find and click on the logout button
+  const logoutButton = await page.$('text=Sign Out');
+  expect(logoutButton).toBeTruthy();
+  await logoutButton.click();
+
+  // Check if the user is redirected to the login page
+  await page.waitForURL(
+    'https://transaction-classification-bot.vercel.app/api/auth/signin?callbackUrl=%2F'
   );
+});
+
+// TEST REQUIRES USER TO PUT IN A await page.pause() COMMAND TO MANUALLY VERIFY PHONE / EMAIL ONE TIME BEFORE THE AUTHENTICATION SKIPS 2FA CHECK.
+
+test('User signs in and redirects to home in deployed', async ({ page }) => {
+  const info = {
+    USER_EMAIL: config.USER_EMAIL,
+    USER_PASSWORD: config.USER_PASSWORD,
+  };
+  await authenticate(page, info);
 });
