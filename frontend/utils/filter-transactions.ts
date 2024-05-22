e9ad0cd36789292b470dd3a3a347861bb2537913
Braden R <brogers42@my.bcit.ca>
2024-05-22 11:04:19 -0700

    Update documentation of middleware and utils files.

--- frontend/utils/filter-transactions.ts
+++ frontend/utils/filter-transactions.ts
@@ -3,6 +3,7 @@ import { Transaction } from '@/types/Transaction';
 export const filterUncategorized = (
   purchases: Transaction[]
 ): Transaction[] => {
+  // Filter out all transactions that do not have uncategorized in the category.
   const filteredPurchases = purchases.filter((purchase: Transaction) =>
     purchase.category.toLowerCase().includes('uncategor')
   );
@@ -10,6 +11,7 @@ export const filterUncategorized = (
 };
 
 export const filterCategorized = (purchases: Transaction[]): Transaction[] => {
+  // Filter out all transactions that have uncategorized in the category.
   const filteredPurchases = purchases.filter(
     (purchase: Transaction) =>
       !purchase.category.toLowerCase().includes('uncategor')
