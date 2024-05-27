7af2a5e4e713abfb76a52fd4380521d9869c4837
Mikko Sio <msio1@my.bcit.ca>
2024-05-27 13:20:09 -0700

    removed sort button padding

--- frontend/components/data-table/columns.tsx
+++ frontend/components/data-table/columns.tsx
@@ -12,7 +12,8 @@ const sortableHeader = (
   return (
     <Button
       variant="ghost"
-      onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+      onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      className="p-0">
       {title}
       <ArrowUpDown className="ml-2 h-4 w-4" />
     </Button>
